fbe93d09e5605b13dd33ce0fc7ffea3b4a41a172
Yashas Shah <79524299+IronCodem@users.noreply.github.com>
2022-02-23 10:06:37 -0500

    Create files

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en-UK">
+<head>
+	<meta charset="utf-8">
+	<title>Refreshments Stand</title>
+	<meta name="description" content="Lemonade Stand | Lead4Change - FeedmorE">
+	<meta name="author" content="YashasShah">
+	<meta name="theme-color" content="#0fb40f">
+	<meta name="keywords" content="lemonade,stand,project,life,l4c">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="favicon.ico" rel="icon" />
+	<link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	<header>
+		<div class="cc">
+			<h1>Refreshments Stand</h1>
+			<img height="250px" alt="logo" src="logo.png">
+		</div>
+
+		<h3>Hi 👋</h3>
+		<h4>Info</h4>
+		<p>
+			As you may have worked out by far, we are a local group of students working towards supporting Feedmore. 
+    </p>
+
+		<h4>Other</h4>
+		<p>
+			?
+		</p>
+	</header>
+
+		<div>
+			<h2>Snow Cones</h2>
+
+			<h3>Overview</h3>
+			<p>
+				We all love snow cones and feel that it is the most perfect commodity during warm weather. Well, what if we tell you that we have a vision to give kids the perfect spring/summer weekend refreshment(s).
+			</p>
+			<h3>Links</h3>
+			<p class="links">Website: <a href="/snow-cones.php#">Further Information</a></p>
+		</div>
+
+	<footer>
+		<h2>Contact us</h2>
+		<ul>
+			<li>To contact an executive call: <a href="8045130103">(804) 513 - 0103</a></li>
+		</ul>
+	</footer>
+
+	<script src="script.js"></script>
+</body>
+</html>
